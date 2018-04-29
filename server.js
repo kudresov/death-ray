16fcd8c5e22327642f6d1e3807ba1cd440ff3a7a
Vitalij Kudresov <kudresov@gmail.com>
2018-04-29 18:28:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.post('/files', function(req, res) {
+  const files = [];
+  fs.readdirSync(req.body.location).forEach(file => {
+    files.push(file);
+  });
+
+  res.send(files);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
